fc734db9394c06d31db9e5f5714e7d11727f2d44
oles88 <oles188@meta.ua>
2016-12-13 15:08:04 +0000

    add new file

---
+++ index.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper('#my-swiper-up .swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+var swiper = new Swiper('#my-swiper-down .swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
